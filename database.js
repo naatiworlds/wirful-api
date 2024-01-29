f768f0c6a03162e412fd9a37d698f775a84beff4
naatii <jcorber855@g.educaand.es>
2024-01-29 22:26:17 +0100

    probando conexión base de datos

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 
 const mysqlConection = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'natiworlds',
