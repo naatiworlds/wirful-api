9ab335aff032f5715b5611c5eb883b034f1ba93c
naatii <jcorber855@g.educaand.es>
2024-01-30 21:15:38 +0100

    Intentando arreglar base de datos 3

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+require('dotenv').config(); // Importa las variables de entorno desde el archivo .env
+
+module.exports = {
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_DATABASE: process.env.DB_DATABASE,
+};
--- database.js
+++ database.js
@@ -1,29 +1,36 @@
+// database.js
 const mysql = require('mysql2');
-
+const {
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_DATABASE,
+} = require('./config'); // Cambiar la ruta según la ubicación real
 
 const mysqlConection = mysql.createConnection({
-    host: 'monorail.proxy.rlwy.net',
-    port: 24279,
-    user: 'root',
-    password: 'EB6AF4GB5dab2E2fA5h6g-hEG2f-ADbC',
-    database: 'railway'
-})
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+});
 
 mysqlConection.connect(function (err) {
-    if (err) {
-        console.log(err);
-        return;
-    } else {
-        console.log("bd is connected");
-    }
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log("bd is connected");
+  }
 });
 
 mysqlConection.end((err) => {
-    if (err) {
-        console.error('Error al cerrar la conexión a la base de datos:', err);
-        return;
-    }
-    console.log('Conexión cerrada correctamente');
+  if (err) {
+    console.error('Error al cerrar la conexión a la base de datos:', err);
+    return;
+  }
+  console.log('Conexión cerrada correctamente');
 });
 
-module.exports = mysqlConection
+module.exports = mysqlConection;
